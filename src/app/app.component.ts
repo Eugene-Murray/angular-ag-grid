b47f3b9f246c284901efb0146889fc89cc719a60
Eugene Murray <eugene.murray@gmail.com>
2025-05-17 19:55:59 +0100

    add files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,48 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AgGridAngular } from 'ag-grid-angular'; // Angular Data Grid Component
+import type { ColDef } from 'ag-grid-community'; // Column Definition Type Interface
+
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
+// Register all Community features
+ModuleRegistry.registerModules([AllCommunityModule]);
+
+interface IRow {
+  make: string;
+  model: string;
+  price: number;
+  electric: boolean;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AgGridAngular, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-ag-grid';
+
+  // Row Data: The data to be displayed.
+  rowData: IRow[] = [
+    { make: 'Tesla', model: 'Model Y', price: 64950, electric: true },
+    { make: 'Ford', model: 'F-Series', price: 33850, electric: false },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: false },
+    { make: 'Mercedes', model: 'EQA', price: 48890, electric: true },
+    { make: 'Fiat', model: '500', price: 15774, electric: false },
+    { make: 'Nissan', model: 'Juke', price: 20675, electric: false },
+  ];
+
+  // Column Definitions: Defines & controls grid columns.
+  colDefs: ColDef<IRow>[] = [
+    { field: 'make' },
+    { field: 'model' },
+    { field: 'price' },
+    { field: 'electric' },
+  ];
+
+  defaultColDef: ColDef = {
+    flex: 1,
+  };
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,10 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
+// Register all Community features
+ModuleRegistry.registerModules([AllCommunityModule]);
+
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
